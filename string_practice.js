2b423ac68ab885f5cd47a3e288b7933d2b9ce951
mccrimmonmd <saturnhs@gmail.com>
2023-04-20 11:06:15 -0600

    Made functions more terse (that makes them better, right?)

--- string_practice.js
+++ string_practice.js
@@ -27,61 +27,41 @@ const masticate = (text) => {
 
 const charCounts = (text) => {
   let counts = {}
-  let charArr = [...text].sort()
-  charArr.forEach(c => {
-    if (counts[c] == null) {
-      counts[c] = 1
-    } else {
-      counts[c] += 1
-    }
+  ;[...text].sort().forEach(character => {
+      counts[character] = counts[character] == null ? 1 : counts[character] + 1
   })
   return counts
 }
 
-const charCountsFunc = (text) => {
-  return [...text].sort().reduce((counts, chara) => {
-    if (counts[chara] == null) {
-      counts[chara] = 1
-    } else {
-      counts[chara] += 1
-    }
+const charCountsFunc = (text) => [...text].sort().reduce((counts, character) =>
+  {
+    counts[character] = counts[character] == null ? 1 : counts[character] + 1
     return counts
   }, {})
-}
 
 const wordCounts = (text) => {
-  let textArr = text.split(/\s/)
-  let sorter = (a, b) => {
-    a = a.replace(/[^\w]/g, '')
-    b = b.replace(/[^\w]/g, '')
-    if (a < b) return -1
-    if (b < a) return 1
-    return 0
-  }
-  let sorted = [...textArr].filter(c => c !== '').sort(sorter)
   let counts = {}
-  sorted.forEach(word => {
-    word = word.replace(/[^\w]/g, '')
-    if (counts[word] == null) counts[word] = 1
-    else counts[word] += 1
-  })
-  return counts
-}
-
-const wordCountsFunc = (text) => {
-  let textArr = text.replace(/[^\w]/g, ' ')
+  text.replace(/(?:^|\s)\W+(\w)/g, ' $1')
+    .replace(/(\w)\W+(?:\s|$)/g, '$1 ')
     .split(/\s/)
     .filter(w => w !== '')
     .sort()
-  return textArr.reduce((counts, word) => {
-    if (counts[word] == null) {
-      counts[word] = 1
-    } else {
-      counts[word] += 1
-    }
-    return counts
-  }, {})
+    .forEach(word => {
+      counts[word] = counts[word] == null ? 1 : counts[word] + 1
+    })
+  return counts
 }
+
+const wordCountsFunc = (text) =>
+  text.replace(/(?:^|\s)\W+(\w)/g, ' $1')
+    .replace(/(\w)\W+(?:\s|$)/g, '$1 ')
+    .split(/\s/)
+    .filter(w => w !== '')
+    .sort()
+    .reduce((counts, word) => {
+      counts[word] = counts[word] == null ? 1 : counts[word] + 1
+      return counts
+    }, {})
 
 if (false) { // DEBUG
   masticate()
